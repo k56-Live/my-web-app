e9127c277fb23a999f38cb55a0361c24e3c1d019
k56 Live <139924674+k56-Live@users.noreply.github.com>
2023-07-28 01:27:10 +0530

    Update home_handler.go

--- webapp/internal/handlers/home_handler.go
+++ webapp/internal/handlers/home_handler.go
@@ -1,1 +1,25 @@
+package handlers
 
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type HomeHandler struct {
+	// You can add dependencies or database connections here if required
+}
+
+func NewHomeHandler() *HomeHandler {
+	return &HomeHandler{}
+}
+
+func (h *HomeHandler) Index(w http.ResponseWriter, r *http.Request) {
+	// Implement index page logic here
+	// ...
+}
+
+func (h *HomeHandler) Dashboard(w http.ResponseWriter, r *http.Request) {
+	// Implement dashboard page logic here
+	// ...
+}
