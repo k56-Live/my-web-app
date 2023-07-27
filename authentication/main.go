8add4324506008ece1cbcf50f6b59e745a74254a
k56 Live <139924674+k56-Live@users.noreply.github.com>
2023-07-28 01:21:03 +0530

    Update main.go

--- authentication/main.go
+++ authentication/main.go
@@ -1,1 +1,19 @@
 
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/username/my-web-app/authentication/internal/handlers"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/login", handlers.AuthHandler)
+
+	fmt.Println("Authentication service is running on http://localhost:8081")
+	http.ListenAndServe(":8081", r)
+}
