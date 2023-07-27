a744e6ab8de9e0b6436d2b40cd0340089b440d7d
k56 Live <139924674+k56-Live@users.noreply.github.com>
2023-07-28 01:28:02 +0530

    Update script.js

--- webapp/web/static/js/script.js
+++ webapp/web/static/js/script.js
@@ -1,5 +1,1 @@
-// Your JavaScript code goes here
-document.addEventListener("DOMContentLoaded", function () {
-  // DOM is ready, you can perform actions here
-  console.log("DOM is ready!");
-});
+// Contents of script.js
