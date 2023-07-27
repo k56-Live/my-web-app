da765b22f1339d23f26139fdab1bfd1a516ec0a2
k56 Live <139924674+k56-Live@users.noreply.github.com>
2023-07-28 01:23:37 +0530

    Update auth_handler.go

--- authentication/internal/handlers/auth_handler.go
+++ authentication/internal/handlers/auth_handler.go
@@ -1,12 +1,27 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/username/my-web-app/authentication/internal/models"
 )
 
-func AuthHandler(w http.ResponseWriter, r *http.Request) {
-	// Implement authentication logic here
+type AuthHandler struct {
+	// You can add dependencies or database connections here if required
+}
+
+func NewAuthHandler() *AuthHandler {
+	return &AuthHandler{}
+}
+
+func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request) {
+	// Implement login logic here
 	// ...
 }
 
+func (h *AuthHandler) Logout(w http.ResponseWriter, r *http.Request) {
+	// Implement logout logic here
+	// ...
+}
