bd602a66a554ea386dea15e04a45d9dce3bdcb22
k56 Live <139924674+k56-Live@users.noreply.github.com>
2023-07-28 00:49:55 +0530

    Update main.go

--- main.go
+++ main.go
@@ -3,40 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/k56-Live/my-web-app/internal/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/login", loginHandler)
-	http.HandleFunc("/dashboard", dashboardHandler)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", handlers.HomeHandler)
+	r.HandleFunc("/login", handlers.LoginHandler)
+	r.HandleFunc("/dashboard", handlers.AuthMiddleware(handlers.DashboardHandler))
 
 	fmt.Println("Server is running on http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to My Web App!")
-}
-
-func loginHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		http.ServeFile(w, r, "./templates/login.html")
-		return
-	}
-
-	username := r.FormValue("username")
-	password := r.FormValue("password")
-
-	// In a real application, you would validate the username and password against a database
-	// For simplicity, we'll use a hardcoded username and password here
-	if username == "admin" && password == "admin123" {
-		http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
-		return
-	}
-
-	http.Redirect(w, r, "/login", http.StatusSeeOther)
-}
-
-func dashboardHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the Dashboard!")
+	http.ListenAndServe(":8080", r)
 }
