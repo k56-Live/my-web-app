7a151ab57733be7bd6d27e7ae672498ebf3e6a68
k56 Live <139924674+k56-Live@users.noreply.github.com>
2023-07-28 01:22:00 +0530

    Update main.go

--- webapp/main.go
+++ webapp/main.go
@@ -1,1 +1,19 @@
+package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/username/my-web-app/webapp/internal/handlers"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", handlers.HomeHandler)
+	r.HandleFunc("/dashboard", handlers.DashboardHandler)
+
+	fmt.Println("Web application service is running on http://localhost:8080")
+	http.ListenAndServe(":8080", r)
+}
